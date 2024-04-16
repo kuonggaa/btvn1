3263d294a731600ea24a4e972fac96e7ea8aafef
kuonggaa <96900121+kuonggaa@users.noreply.github.com>
2024-04-16 14:30:24 +0700

    Update LoopExample.java

--- b1.java
+++ b1.java
@@ -1,10 +1,9 @@
-
 public class LoopExample {
-    public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 10; i++) {
-            sum += i;
+    public static int sum(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
         }
-        System.out.println("Tổng của các số từ 1 đến 10 là: " + sum);
+        return result;
     }
 }
