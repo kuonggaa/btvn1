855a6569866c68bcd28dd23ef26f47782faa0c72
kuonggaa <96900121+kuonggaa@users.noreply.github.com>
2024-04-16 14:00:45 +0700

    Create b1.java

---
+++ b1.java
@@ -0,0 +1,10 @@
+cho.import java.util.Scanner;
+public class LoopExample {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Tổng của các số từ 1 đến 10 là: " + sum);
+    }
+}
