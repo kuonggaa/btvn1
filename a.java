0fc00c7d3b4c0bc889b52f6bd3401ccc9fb71096
kuonggaa <96900121+kuonggaa@users.noreply.github.com>
2024-04-16 13:28:01 +0700

    Update a.java

--- a.java
+++ a.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập một số nguyên: ");
+        System.out.print("Nhập một số nguyên dương: ");
         int number = scanner.nextInt();
 
-        // Sử dụng lệnh rẽ nhánh để kiểm tra số nhập vào là chẵn hay lẻ
-        if (number % 2 == 0) {
-            System.out.println("Số bạn nhập vào là số chẵn.");
+        // Sử dụng phương thức để kiểm tra số nhập vào là số nguyên tố hay không
+        if (isPrime(number)) {
+            System.out.println(number + " là số nguyên tố.");
         } else {
-            System.out.println("Số bạn nhập vào là số lẻ.");
+            System.out.println(number + " không phải là số nguyên tố.");
         }
 
         // Sử dụng vòng lặp để in ra các số từ 1 đến số nhập vào
@@ -18,5 +18,18 @@ public class Main {
         for (int i = 1; i <= number; i++) {
             System.out.print(i + " ");
         }
+    }
+
+    // Phương thức kiểm tra một số có phải là số nguyên tố hay không
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
